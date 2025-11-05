2e6ce25b03bfd2b5476bf9645922b66188c5e795
agustinso0 <loosagustin@gmail.com>
2025-11-05 20:19:59 -0300

    refactor: reorganizar importaciones y mejorar comentarios en setupTests.ts

--- TP3/cafeteria/src/setupTests.ts
+++ TP3/cafeteria/src/setupTests.ts
@@ -1,8 +1,8 @@
+import "@testing-library/jest-dom";
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./mocks/server";
 
-import '@testing-library/jest-dom';
-import { server } from './mocks/server'; // Asegúrate de crear y exportar 'server' aquí
-
-//hooks de Vitest para gestionar MSW
+// Hooks de Vitest para gestionar MSW
 beforeAll(() => server.listen()); // Iniciar el servidor de MSW antes de todas las pruebas
 afterEach(() => server.resetHandlers()); // Resetear los handlers después de cada prueba
 afterAll(() => server.close()); // Cerrar el servidor de MSW cuando todas las pruebas terminan
