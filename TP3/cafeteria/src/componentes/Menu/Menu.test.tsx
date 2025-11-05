57d41fe9239fea6c36ad7d0f8bf79ef327e10533
agustinso0 <loosagustin@gmail.com>
2025-11-05 20:35:36 -0300

    refactor: mejorar la legibilidad y consistencia en Menu.test.tsx

--- TP3/cafeteria/src/componentes/Menu/Menu.test.tsx
+++ TP3/cafeteria/src/componentes/Menu/Menu.test.tsx
@@ -1,16 +1,20 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { expect, test, describe } from 'vitest';
-import { Menu } from './Menu'; 
-import { PedidoProvider } from '../../contexto/PedidoContexto'; 
+import { render, screen } from "@testing-library/react";
+import { expect, test, describe } from "vitest";
+import { Menu } from "./Menu";
+import { PedidoProvider } from "../../contexto/PedidoContexto";
 
-describe('HU1 y HU2: Interacción del Menú', () => {
+describe("HU1 y HU2: Interacción del Menú", () => {
+  test("✓ HU1: Debe mostrar el listado de productos después de cargarlos de la API", async () => {
+    render(
+      <PedidoProvider>
+        <Menu />
+      </PedidoProvider>
+    );
 
-    test('✓ HU1: Debe mostrar el listado de productos después de cargarlos de la API', async () => {
-        render(<PedidoProvider><Menu /></PedidoProvider>);
-
-        expect(screen.getByText(/Cargando menú.../i)).toBeInTheDocument();
-        expect(await screen.findByRole('heading', { name: /Espresso/i })).toBeInTheDocument(); 
-        expect(screen.queryByText(/Cargando menú.../i)).not.toBeInTheDocument();
-    });
-
+    expect(screen.getByText(/Cargando menu.../i)).toBeInTheDocument();
+    expect(
+      await screen.findByRole("heading", { name: /Espresso/i })
+    ).toBeInTheDocument();
+    expect(screen.queryByText(/Cargando menu.../i)).not.toBeInTheDocument();
+  });
 });
