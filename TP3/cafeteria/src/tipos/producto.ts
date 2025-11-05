c5ec5308f1ee9c614825618ff318c3d1a91e04d0
agustinso0 <loosagustin@gmail.com>
2025-11-05 20:34:41 -0300

    refactor: corregir errores tipográficos y mejorar la consistencia en producto.ts

--- TP3/cafeteria/src/tipos/producto.ts
+++ TP3/cafeteria/src/tipos/producto.ts
@@ -1,24 +1,24 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const ProductoSchema = z.object({
   id: z.string(),
-  nombre: z.string().min(2), 
+  nombre: z.string().min(2),
   precio: z.number().positive(),
-  tipo: z.enum(['Bebida', 'Comida']), 
+  tipo: z.enum(["Bebida", "Comida"]),
 });
 
-export type Producto = z.infer<typeof ProductSchema>;
+export type Producto = z.infer<typeof ProductoSchema>;
 
 export interface OrderItem {
-    id: string;
-    name: string;
-    price: number;
-    quantity: number;
-};
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
 
 export interface ItemPedido {
-    id: string;
-    nombre: string;
-    precio: number;
-    cantidad: number;
+  id: string;
+  nombre: string;
+  precio: number;
+  cantidad: number;
 }
