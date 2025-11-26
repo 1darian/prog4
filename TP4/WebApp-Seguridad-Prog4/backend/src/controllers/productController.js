591470eb0910b10e26f119d947cb7b0016abf248
JuaniMartinezHeguy <juanitomh487@gmail.com>
2025-11-26 20:54:45 -0300

    test-07-completo

--- TP4/WebApp-Seguridad-Prog4/backend/src/controllers/productController.js
+++ TP4/WebApp-Seguridad-Prog4/backend/src/controllers/productController.js
@@ -1,28 +1,29 @@
-const { db } = require('../config/database');
+ const { db } = require('../config/database');
 
-// VULNERABLE: SQL Injection
-const getProducts = (req, res) => {
-  const { category, search } = req.query;
-  
-  // VULNERABLE: Concatenación directa de strings en SQL
-  let query = 'SELECT * FROM products WHERE 1=1';
-  
-  if (category) {
-    query += ` AND category = '${category}'`;
-  }
-  
-  if (search) {
-    query += ` AND name LIKE '%${search}%'`;
-  }
-  
-  db.query(query, (err, results) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json(results);
-  });
-};
+ const getProducts = (req, res) => {
+   const { category, search } = req.query;
 
-module.exports = {
-  getProducts
-};
+   let query = 'SELECT id, name, category, price, stock FROM products WHERE 1=1';
+   const params = [];
+
+   if (category) {
+     query += ' AND category = ?';
+     params.push(category);
+   }
+
+   if (search) {
+     query += ' AND name LIKE ?';
+     params.push('%' + search + '%');
+   }
+
+   db.query(query, params, (err, results) => {
+     if (err) {
+       return res.status(200).json([]);
+     }
+     res.json(results);
+   });
+ };
+
+ module.exports = {
+   getProducts
+ };
